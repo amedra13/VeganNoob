2e48f47d5b4b83530f9801d844c140f00546e86b
Andres Medrano <amedra13@gmail.com>
2020-12-31 00:07:11 -0800

    begin recipe component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/main.scss';
 import BrandImage from './BrandImage';
+import Recipe from './Recipe';
 
 function App() {
 	return (
 		<div className="app">
 			<BrandImage />
+			<div className="appContent">
+				<Recipe />
+			</div>
 		</div>
 	);
 }
--- src/BrandImage.js
+++ src/BrandImage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Icon from './images/icon.png';
 
 const BrandImage = () => {
 	return (
 		<div className="brandImage">
-			<h1>VeganNoob</h1>
-			<h4>Welcome to my website!</h4>
+			<div>
+				<img src={Icon} alt="placeholder" />
+			</div>
+			<div>
+				<h1>VeganNoob</h1>
+				<h4>Welcome to my website!</h4>
+			</div>
+			<div className="links">
+				<h4>Recipes</h4>
+				<h4>About</h4>
+				<h4>Contact</h4>
+			</div>
 		</div>
 	);
 };
