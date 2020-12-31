412c05d7772746c018da2eee8b93b09b370283eb
Andres Medrano <amedra13@gmail.com>
2020-12-30 23:46:10 -0800

    CRA and structure stylesheets

---
+++ src/BrandImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BrandImage = () => {
+	return (
+		<div className="brandImage">
+			<h1>VeganNoob</h1>
+			<h4>Welcome to my website!</h4>
+		</div>
+	);
+};
+
+export default BrandImage;
