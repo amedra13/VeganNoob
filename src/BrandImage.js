d72c449bede04a7fb726052369fc985311be0e0c
Andres Medrano <amedra13@gmail.com>
2021-01-13 18:25:09 -0800

    app header now responsive

--- src/BrandImage.js
+++ src/BrandImage.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Icon from './images/icon.png';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const BrandImage = () => {
 	let home = useLocation().pathname;
+	const matches = useMediaQuery('(max-width:600px)');
 
 	const backgroundStyle = home === '/' ? 'transparent' : 'pink';
 
@@ -22,13 +25,19 @@ const BrandImage = () => {
 				<h1>Loi's Version</h1>
 			</Link>
 			<div className="brandImage__links">
-				<Link to="/recipes">
-					<h4>Recipes</h4>
-				</Link>
-				<h4>About</h4>
-				<Link to="/contact">
-					<h4>Contact</h4>
-				</Link>
+				{matches ? (
+					<MenuIcon />
+				) : (
+					<>
+						<Link to="/recipes">
+							<h4>Recipes</h4>
+						</Link>
+						<h4>About</h4>
+						<Link to="/contact">
+							<h4>Contact</h4>
+						</Link>
+					</>
+				)}
 			</div>
 		</div>
 	);
